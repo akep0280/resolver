3618d764d5c3348ad24ca312972d0334d11df495
akep0280 <akepley@gmail.com>
2015-07-29 15:27:00 -0500

    Initial Commit

---
+++ manifests/config.pp
@@ -0,0 +1,12 @@
+class resolver::config {
+  #install DNS resolv.conf
+  file {'/etc/resolv.conf':
+    ensure => 'file',
+    owner  => 'root'
+    group  => 'root',
+    mode   => '0644',
+    source => $::ipaddress ? {
+      '^172\.16\.' => 'puppet:///modules/resolver/cerberus.conf',
+      '^172\.17\.' => 'puppet:///modules/resolver/dns01.conf',
+      '^192\.168\.'  => 'puppet:///modules/resolver/anubis.conf',
+}
---
+++ manifests/init.pp
@@ -0,0 +1,3 @@
+class resolver {
+  include resolver::config
+}
